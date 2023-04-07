27b902cfa413a7ac33f4e46b6eb5dd13313755ed
sano-nakaji <s205022_rec@outlook.jp>
2023-04-08 07:30:43 +0900

    現在地情報取得と周辺店舗表示機能の実装

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -18,24 +18,25 @@ class SearchController < ApplicationController
   #   end
   # end
 
-def shops
-  latitude = params[:latitude].to_f #定数を入れると正常に動作する 35.4648064
-  longitude = params[:longitude].to_f #133.070848
+  def shops
+    latitude = params[:latitude].to_f #定数を入れると正常に動作する 35.4648064
+    longitude = params[:longitude].to_f #133.070848
   
-  client = HotpepperClient.new(latitude, longitude)
-  @shops = client.search_shops(latitude, longitude)
+    client = HotpepperClient.new(latitude, longitude)
+    @shops = client.search_shops(latitude, longitude)
 
-  @latitude = latitude
-  @longitude = longitude
+    @latitude = latitude
+    @longitude = longitude
 
-  respond_to do |format|
-    format.html # クライアントがHTMLを要求した場合、上記で作成したビューを表示
-    format.json { render json: @shops } # クライアントがJSONを要求した場合は、JSON形式でデータを返す
-  end
+    @test = 20000
 
-  # debug
-  # client = HotpepperClient.new(latitude, longitude)
-  # @shops = client.search_shops(latitude, longitude)
-  # puts @shops.inspect
+    respond_to do |format|
+      format.html # クライアントがHTMLを要求した場合、上記で作成したビューを表示
+      format.json { render json: @shops } # クライアントがJSONを要求した場合は、JSON形式でデータを返す
+    end
+
+    # debug
+    # client = HotpepperClient.new(latitude, longitude)
+    # @shops = client.search_shops(latitude, longitude)
   end
 end
